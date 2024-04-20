8a37cad53667353dc31337c1f90fb15e1ee432b0
Hasi2005 <prathapahasitha@gmail.com>
2024-04-20 14:42:11 +0530

    First commit

---
+++ Raaga/public/js/script.js
@@ -0,0 +1,46 @@
+
+const button1 = document.querySelector(".badge.hide");
+button1.addEventListener('click', function () {
+   
+    window.location.href = '/another'; 
+});
+
+
+
+// const button2 = document.querySelector("#art");
+
+// button2.addEventListener('click', function () {
+//     window.location.href = '/artist'; 
+// });
+
+// const button3 = document.querySelector("#mood");
+
+// button3.addEventListener('click', function () {
+//     window.location.href = '/mood'; 
+// });
+// const button4 = document.querySelector("#genre");
+
+// button4.addEventListener('click', function () {
+//     window.location.href = '/genre'; 
+// });
+
+const button5 = document.querySelector("#register");
+button5.addEventListener('click', function () {
+    window.location.href = '/register'; 
+});
+
+const button6 = document.querySelector("#play");
+button6.addEventListener('click', function () {
+    window.location.href = '/playlist'; 
+});
+
+const button7 = document.querySelector("#podcast");
+button7.addEventListener('click', function () {
+    window.location.href = '/podcast'; 
+});
+
+
+    const button8 = document.querySelector("#login");
+button8.addEventListener('click', function () {
+    window.location.href = '/login'; 
+});
---
+++ Raaga/public/js/search_sc.js
@@ -0,0 +1,9 @@
+const ele=document.querySelector(".search");
+// ele.addEventListener('input',()=>{
+//     console.log(ele.value);
+// });
+const ele2=document.querySelector("#sub");
+ele2.addEventListener('click',()=>{
+    let data=ele.value;
+    console.log(data);
+});
